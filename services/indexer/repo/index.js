a3e71e724941c8b4e998eaf6262279cefd12afad
Michael Balint <michael.balint@pif.gov>
2016-12-19 14:54:41 -0500

    Added some TODO notes.

--- services/indexer/repo/index.js
+++ services/indexer/repo/index.js
@@ -126,6 +126,9 @@ class AgencyJsonStream extends Transform {
               }
               // only push if we haven't encountered errors
               this.push(formattedProject);
+              // write the file to the `fetched`
+              // TODO: add logic here
+              // TODO: create an elasticsearch query response reader
               return done();
             });
           });
