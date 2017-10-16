ab2857b8db0f3aa54257bbf2a6952ec1f280182d
Froilan Irizarry <froilan@froilanirizarry.me>
2017-10-16 17:20:02 -0400

    Change time to live for indexes - Changed the time to live for ES indexes to get value from environment vars. - Added a default time to live for ES indexes of 2 days. This should reduce the amount of space we are consuming.

--- scripts/index/repo/index.js
+++ scripts/index/repo/index.js
@@ -8,7 +8,7 @@ const IndexOptimizer        = require("../../../services/indexer/index_optimizer
 const Logger                = require("../../../utils/logger");
 const elasticsearchAdapter  = require("../../../utils/search_adapters/elasticsearch_adapter");
 
-const DAYS_TO_KEEP = 7;
+const DAYS_TO_KEEP = process.env.DAYS_TO_KEEP || 2;
 const AGENCY_ENDPOINTS_FILE = path.join(__dirname, "../../../", config.AGENCY_ENDPOINTS_FILE);
 
 /**
