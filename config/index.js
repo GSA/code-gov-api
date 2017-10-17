afabf61c16a6d8e57f30089fbed3c066c6e97f4e
Froilan Irizarry <froilan@froilanirizarry.me>
2017-10-16 23:10:54 -0400

    Add cfenv and refactor elasticsearch config - Added cfenv to use environment variables provided by cloud.gov. This will permit us to connect to services dynamically. - Configure elasticsearch outside of config files using cfenv values. - Added dummy New Relic key when app is not in production env to avoid key error.

--- config/index.js
+++ config/index.js
@@ -1,11 +1,20 @@
 /* eslint-disable */
-var CONFIG;
+const cfenv = require('cfenv')
+const appEnv = cfenv.getAppEnv()
+const elasticSearchCredentials = appEnv.getServiceCreds("cod_gov_elasticsearch")
+
+let config;
 
 if (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "production") {
-  CONFIG = require("./prod/index.json");
+  config = require("./prod/index.json");
+  config.ES_AUTH = `${elasticSearchCredentials.username}:${elasticSearchCredentials.password}`
+  config.ES_HOST = elasticSearchCredentials.hostname
+  config.ES_PORT = elasticSearchCredentials.port
 } else {
-  CONFIG = require("./dev/index.json");
+  config = require("./dev/index.json");
+  process.env.nrkey = "dont-tell-eve"
 }
 
-module.exports = CONFIG;
+
+module.exports = config;
 /* eslint-enable */
