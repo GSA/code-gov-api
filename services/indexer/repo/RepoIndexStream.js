bf08069e1037597336458f1030cc7f6d940a4963
Froilan Irizarry <froilan@froilanirizarry.me>
2017-10-24 02:46:46 -0400

    Initial commit of services/indexer/repo/RepoIndexStream.js

---
+++ services/indexer/repo/RepoIndexStream.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const path = require('path');
+const { Writable } = require("stream");
+
+class RepoIndexerStream extends Writable {
+  constructor(indexer) {
+    super({
+      objectMode: true
+    });
+    this.indexer = indexer;
+  }
+
+
+  _indexRepo(repo) {
+    return new Promise((fulfill, reject) => {
+      // TODO: turn this call into a promise
+      this.indexer.indexDocument({
+        "index": this.indexer.esIndex,
+        "type": this.indexer.esType,
+        "id": repo.repoID,
+        "body": repo
+      }, (err, response, status) => {
+        if(err) {
+          this.logger.error(err);
+          reject(err);
+        } else {
+          if (status) {
+            this.logger.debug('Status', status);
+          }
+          this.repoIndexer.indexCounter++;
+    
+          fulfill({status, response});
+        }
+      });
+    });
+  }
+
+  _write(repo, enc, next) {
+    this._indexRepo(repo)
+      .then((status, response) => {
+        return next(null, response);
+      })
+      .catch(err => {
+        this.logger.error(err);
+        return next(err, null);
+      });
+  }
+}
+
+module.exports = RepoIndexerStream;
