6c4b5047e699eeeb15b7d9723dcdbb60e6bef243
Froilan Irizarry <froilan@froilanirizarry.me>
2017-10-30 19:47:02 -0400

    Added validator and formatter servises

--- services/indexer/repo/AgencyJsonStream.js
+++ services/indexer/repo/AgencyJsonStream.js
@@ -6,6 +6,11 @@ const request = require("request");
 const Jsonfile = require("jsonfile");
 const Logger = require('../../../utils/logger')
 
+const Validator = require('../../validator');
+const Formatter = require('../../formatter');
+const Reporter = require("../../reporter");
+const Utils = require("../../../utils");
+
 const logger = new Logger({name: 'agency-json-stream'});
 const fetchedDir = path.join(__dirname, '../../../data/fetched/');
 
@@ -49,61 +54,127 @@ class AgencyJsonStream extends Transform {
         url: agency.codeUrl,
         headers: {
           'User-Agent': 'code.gov',
-          'Content-Type': 'application/json'
         }
       };
   
       request(requestParams, (err, response, body) => {
+        const errorMessage = 'FAILURE: There was an error fetching the code.json:';
         if(err) {
-          reject(`${agency.codeUrl} - ${err}`);
+          reject(`${errorMessage} ${agency.codeUrl} - ${err}`);
         } else {
           if(response.statusCode === 200) {
             const formattedData = body.replace(/^\uFEFF/, '');
   
             this._saveFetchedCodeJson(agency.acronym, formattedData)
-              .then(data => {
-                fulfill(data);
-              })
-              .catch(err => {
-                reject(`${agency.codeUrl} - ${err}`);
-              });
+              .then(data => fulfill(data))
+              .catch(err => reject(`errorMessage ${agency.codeUrl} - ${err}`));
           } else {
-            reject(`${agency.codeUrl} returned ${response.statusCode}`);
+            reject(`${errorMessage} ${agency.codeUrl} returned ${response.statusCode}`);
           }
         }
       });
     });
   }
 
-  _validateCodeJson(codeJson) {
-    logger.debug('Entered _validateCodeJson - Agency: ', codeJson.agency);
+  _validateAgencyRepos(agency, codeJson) {
+    logger.debug('Entered _validateAgencyRepos');
 
-    return new Promise((fulfill, reject) => {
-      // get validator for code.json version
-      fulfill(codeJson);
+    let reportDetails = [];
+    let reportString = "";
+    let totalErrors = 0;
+    let validationTotals = {
+      errors: 0,
+      warnings: 0,
+      enhancements: 0
+    };
+
+    Reporter.reportVersion(agency.acronym, codeJson.version);
+
+    let repos = codeJson.projects.map(repo => {
+      const repoId = Utils.transformStringToKey([agency.acronym, repo.organization, repo.name].join("_"));
+
+      Validator.validateRepo(repo, (error, results) => {
+        if(error) {
+          logger.error(`Error validating repo with repoID ${repoId}. Throwing it out of the indexing process.`);
+        } else {
+          if(results.issues) {
+            validationTotals.errors += results.issues.errors.length ? results.issues.errors.length : 0;
+            validationTotals.warnings += results.issues.warnings.length ? results.issues.warnings.length : 0;
+            validationTotals.enhancements += results.issues.enhancements.length ? results.issues.enhancements.length : 0;
+  
+            Reporter.reportIssues(agency.acronym, results);
+          }
+
+          return repo;
+        }
+      });
     });
+
+    if(validationTotals.errors) {
+      totalErrors += validationTotals.errors;
+      reportDetails.push(`${validationTotals.errors} ERRORS`);
+    }
+    if(validationTotals.warnings) {
+      totalErrors += validationTotals.warnings;
+      reportDetails.push(`${validationTotals.warnings} WARNINGS`);
+    }
+
+    if(validationTotals.enhancements) {
+      reportDetails.push(`${validationTotals.enhancements} REQUESTED ENHANCEMENTS`);
+    }
+
+    if(totalErrors) {
+      reportString= "NOT FULLY COMPLIANT: ";
+    } else {
+      agency.requirements.schemaFormat = 1;
+      reportString= "FULLY COMPLIANT: ";
+    }
+
+    reportString += reportDetails.join(", ");
+    Reporter.reportStatus(agency.acronym, reportString);
+
+    agency.requirements.overallCompliance = this._calculateOverallCompliance(agency.requirements);
+    Reporter.reportRequirements(agency.acronym, agency.requirements);
+    
+    return Promise.resolve(repos);
   }
 
-  _formatCodeJson(codeJson) {
-    logger.debug('Entered _formatCodeJson - Agency: ', codeJson.agency);
-
-    return new Promise((fulfill, reject) => {
-      fulfill(codeJson);
-    });
+  _formatRepos(agency, repos) {
+    logger.debug('Entered _formatCodeJson - Agency: ', agency.acronym);
+    return Promise.all(
+      repos.map(repo => {
+        repo.agency = agency;
+        Formatter.formatRepo(repo, (err, formattedRepo) => {
+          if (err) {
+            const msg = `[Error] Formatting repo: ${repo.name}. Throwing it out of the indexing process.`;
+            logger.error(msg, err);
+          }
+          return formattedRepo;
+        });
+      })
+    );
   }
 
   _transform(agency, enc, callback) {
     logger.debug('Entered _transform - Agency: ', agency.acronym);
+    Reporter.reportMetadata(agency.acronym, { agency });
 
     this._getAgencyCodeJson(agency)
       .then(codeJson => {
-        return this._validateCodeJson(codeJson)
+        if(codeJson.projects) {
+          return this._validateAgencyRepos(agency, codeJson);
+        } else {
+          const message = `ERROR: Agency ${agency.acronym} code.json has no projects.`;
+          Reporter.reportStatus(agency.acronym, message)
+          
+          return Promise.reject(message);
+        }
       })
-      .then(validCodeJson => {
-        return this._formatCodeJson(validCodeJson);
+      .then(repos => {
+        return this._formatRepos(agency, repos)
       })
-      .then(formattedCodeJson => {
-        this.push(formattedCodeJson);
+      .then(formattedRepos => {
+        this.push(formattedRepos);
       })
       .catch(err => {
         logger.error(err);
