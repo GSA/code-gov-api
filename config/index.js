f42d909350eeb7c472154f24e3f97ca477f1ac36
Froilan Irizarry <froilan@froilanirizarry.me>
2017-12-11 12:16:04 -0500

    Refactor. - Imported getProductionConfig and getDevelopmentConfig functions. - Moved Elasticsearch settings to prod and dev config modules. - Moved cloud foundry / cloud.gov settings and imports to prod config module. - Paths to data directories are now set before exporting config. This will let us have more consistent file paths and help us make our modules more testable.

--- config/index.js
+++ config/index.js
@@ -1,20 +1,20 @@
-/* eslint-disable */
-const cfenv = require('cfenv')
-const appEnv = cfenv.getAppEnv()
-const elasticSearchCredentials = appEnv.getServiceCreds("code_gov_elasticsearch")
-
+const path = require('path');
+const getProductionConfig = require('./prod');
+const getDevelopmentConfig = require('./dev');
 let config;
 
 if (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "production") {
-  config = require("./prod/index.json");
-  config.ES_AUTH = `${elasticSearchCredentials.username}:${elasticSearchCredentials.password}`
-  config.ES_HOST = elasticSearchCredentials.hostname
-  config.ES_PORT = elasticSearchCredentials.port
+  config = getProductionConfig();
 } else {
-  config = require("./dev/index.json");
-  process.env.nrkey = "dont-tell-eve"
+  config = getDevelopmentConfig();
+  process.env.nrkey = config.NR_KEY;
 }
 
+config.AGENCY_ENDPOINTS_FILE = path.join(path.dirname(__dirname), config.AGENCY_ENDPOINTS_FILE);
+config.REPORT_FILEPATH = path.join(path.dirname(__dirname), config.REPORT_FILEPATH);
+config.DISCOVERED_DIR = path.join(path.dirname(__dirname), config.DISCOVERED_DIR);
+config.FETCHED_DIR = path.join(path.dirname(__dirname), config.FETCHED_DIR);
+config.DIFFED_DIR = path.join(path.dirname(__dirname), config.DIFFED_DIR);
+config.FALLBACK_DIR = path.join(path.dirname(__dirname), config.FALLBACK_DIR);
 
 module.exports = config;
-/* eslint-enable */
