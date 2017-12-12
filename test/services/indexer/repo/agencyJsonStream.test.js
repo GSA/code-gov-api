14e42e94bdc49fac74b59be08bba78a7a12e6c9b
Froilan Irizarry <froilan@froilanirizarry.me>
2017-12-11 21:26:53 -0500

    Clean up tests with promises and updated repo expected format. - expected format for repo in Should return formatted repos test was updated to schema 2.0. - Removed done function/parameter where it is not needed. Basically just returned the promises in the tests.

--- test/services/indexer/repo/agencyJsonStream.test.js
+++ test/services/indexer/repo/agencyJsonStream.test.js
@@ -23,52 +23,52 @@ describe('AgencyJsonStream', function() {
     agencyJsonStream = new AgencyJsonStream(fetchDataDir, fallbackDataDir);
   });
 
-  it('Should save codeJson to disk', function(done) {
+  it('Should save codeJson to disk', function() {
     const codeJson = JsonFile.readFileSync(path.join(fallbackDataDir, '/FAKE.json'));
 
-    agencyJsonStream._saveFetchedCodeJson('FAKE', codeJson)
+    return agencyJsonStream._saveFetchedCodeJson('FAKE', codeJson)
       .then(data =>{
         const fetchedCodeJson = JsonFile.readFileSync(path.join(fetchDataDir, '/FAKE.json'));
         data.should.be.deep.equal(fetchedCodeJson);
-      })
-      .then(done, done);
+      });
   });
 
-  it('Should return codejson from disk', function(done) {
+  it('Should return codejson from disk', function() {
     const codeJson = JsonFile.readFileSync(path.join(fallbackDataDir, '/FAKE.json'));
-    agencyJsonStream._getAgencyCodeJson(agency[0])
+    return agencyJsonStream._getAgencyCodeJson(agency[0])
       .then(data => {
         data.should.be.deep.equal(codeJson);
-      })
-      .then(done, done);
+      });
   });
 
-  it('Should return array of repos', function(done){
+  it('Should return array of repos', function(){
     const codeJson = JsonFile.readFileSync(path.join(fallbackDataDir, '/FAKE.json'));
     const expectedRepos = codeJson.projects;
-    agencyJsonStream._validateAgencyRepos(agency[0], codeJson)
-      .then(repos => {
-        repos.should.be.deep.equal(expectedRepos);
-      })
-      .then(done, done);
+    return agencyJsonStream._validateAgencyRepos(agency[0], codeJson)
+      .then(result => {
+        result.repos.should.be.deep.equal(expectedRepos);
+      });
   });
 
-  it('Should return fomatted repos', function(done){
+  it.only('Should return fomatted repos', function(done){
     const expectedRepo = {
       name: 'Save Mail',
       organization: 'FAKE_ORG',
       description: 'FAKE Gmail extension to save messages to local disk',
-      license: null,
-      openSourceProject: 0,
-      governmentWideReuseProject: 0,
+      permissions: {
+        licenses: [{
+          URL: '',
+          name: ''
+        }],
+        usageType: 'openSource',
+        exemptionText: null
+      },
       tags: ['save', 'mail'],
       contact: { email: 'fake@fake.gov', name: 'Derp Fakerson', phone: '5555555555' },
       status: 'Production',
       vcs: 'git',
-      homepage: 'https://github.com/FAKEGOV/FAKEMailSaveMessageExtension-',
+      homepageURL: 'https://github.com/FAKEGOV/FAKEMailSaveMessageExtension-',
       downloadURL: 'https://github.com/FAKE_ORG/FAKEMailSaveMessageExtension-/archive/master.zip',
-      exemption: null,
-      exemptionText: 'No exemption requested',
       updated: { lastCommit: '2017-04-11T00:00:00.000Z', metadataLastUpdated: '2017-04-22T00:00:00.000Z', lastModified: '2017-04-11' },
       agency: {
         name: 'Department of FAKE',
@@ -84,13 +84,9 @@ describe('AgencyJsonStream', function() {
     const jsonStream = JsonStream.parse('*');
 
     agencyStream.pipe(jsonStream).pipe(agencyJsonStream).on('data', (repos) => {
-      try {
-        repos.should.be.deep.equal(expectedRepo);
-        done();
-      }catch(err) {
-        done(err);
-      }
-    });
+      repos.should.be.deep.equal(expectedRepo);
+      done();
+    }).on('error', (error) => done(error));
   });
 
   describe('Compliance Calculations', function() {
