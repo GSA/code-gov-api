4e2bdbb0a6323285eac711f7cafa1fd7fcbad1ab
Froilan Irizarry <froilan@froilanirizarry.me>
2018-01-29 14:46:37 -0500

    Added github label regular expressions to config

--- config/index.js
+++ config/index.js
@@ -16,5 +16,10 @@ config.DISCOVERED_DIR = path.join(path.dirname(__dirname), config.DISCOVERED_DIR
 config.FETCHED_DIR = path.join(path.dirname(__dirname), config.FETCHED_DIR);
 config.DIFFED_DIR = path.join(path.dirname(__dirname), config.DIFFED_DIR);
 config.FALLBACK_DIR = path.join(path.dirname(__dirname), config.FALLBACK_DIR);
+config.issueTypeRegEx = /\[issue-type\]/;
+config.skillLevelRegEx = /\[skill-level\]/;
+config.effortRegEx = /\[effort\]/;
+config.impactRegEx = /\[impact\]/;
+config.featuredRegEx = /\[featured\]/;
 
 module.exports = config;
