275da30caffef6967e61a59894bc12a9e0905514
Froilan Irizarry <froilan@froilanirizarry.me>
2018-01-31 12:29:09 -0500

    Added github integration config values

--- config/index.js
+++ config/index.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const getProductionConfig = require('./prod');
 const getDevelopmentConfig = require('./dev');
+const dotenv = require('dotenv');
+
 let config;
 
 if (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "production") {
   config = getProductionConfig();
 } else {
+  dotenv.config();
   config = getDevelopmentConfig();
   process.env.nrkey = config.NR_KEY;
 }
@@ -16,10 +19,13 @@ config.DISCOVERED_DIR = path.join(path.dirname(__dirname), config.DISCOVERED_DIR
 config.FETCHED_DIR = path.join(path.dirname(__dirname), config.FETCHED_DIR);
 config.DIFFED_DIR = path.join(path.dirname(__dirname), config.DIFFED_DIR);
 config.FALLBACK_DIR = path.join(path.dirname(__dirname), config.FALLBACK_DIR);
-config.issueTypeRegEx = /\[issue-type\]/;
-config.skillLevelRegEx = /\[skill-level\]/;
-config.effortRegEx = /\[effort\]/;
-config.impactRegEx = /\[impact\]/;
-config.featuredRegEx = /\[featured\]/;
-
+config.ISSUE_TYPE_REGEXP = /\[issue-type\]/;
+config.SKILL_LEVEL_REGEXP = /\[skill-level\]/;
+config.EFFORT_REGEXP = /\[effort\]/;
+config.IMPACT_REGEXP = /\[impact\]/;
+config.FEATURED_REGEXP = /\[featured\]/;
+config.GITHUB_ISSUE_STATE = 'open';
+config.GITHUB_HELP_WANTED_ISSUE_LABELS = '[help wanted],[code.gov]';
+config.GITHUB_AUTH_TYPE = 'token';
+config.GITHUB_TOKEN = process.env.GITHUB_TOKEN || null;
 module.exports = config;
