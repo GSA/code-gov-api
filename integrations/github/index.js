c71b0becd15e4a3d4ac4f054ac82f0aebebec0ef
Froilan Irizarry <froilan@froilanirizarry.me>
2018-01-31 12:27:58 -0500

    Code refactor and github issues. - Refactored code to better use the async wait feature. - Changed config var to options. I do not want to cause confusion with the general app config. - Added _getLabelValue helper function. - Expanded the getRepoInformation return object - built out the getRepoIssues funciton. It now parses labels to extract metadata values for help wanted.

--- integrations/github/index.js
+++ integrations/github/index.js
@@ -3,59 +3,123 @@ const Octokit = require('@octokit/rest');
 function _parseGithubUrl(githubUrl) {
   const githubUrlRegEx = /github.com/
   const match = githubUrl.match(githubUrlRegEx);
-  let githubUser;
-  let repoName;
 
   if (match) {
-    githubUser = match['input'].split('/')[3];
-    repoName = match['input'].split('/')[4];
+    const githubUserAndRepo = match['input'].split('/').slice(-2);
+    return { 
+      githubUser: githubUserAndRepo[0],
+      repoName: githubUserAndRepo[1]
+    };
   } else {
     throw new Error('URL is not a valid Github URL.');
   }
-  return { githubUser, repoName }
 }
 
-function _getGithubClient(config) {
+function _getGithubClient(options) {
   const octokit = Octokit();
 
   octokit.authenticate({
-    type: config.authType,
-    token: config.githubToken
+    type: options.GITHUB_AUTH_TYPE,
+    token: options.GITHUB_TOKEN
   });
 
   return octokit;
 }
 
-async function getRepoInformation(repoUrl, config) {
-  const githubClient = _getGithubClient(config);
+function _getLabelValue(label, regExp) {
+  let match = label.match(regExp);
+  return match['input'].split(' ')[1] || null;
+}
+
+async function getRepoInformation(repoUrl, options) {
+  const githubClient = _getGithubClient(options);
   
   const { githubUser, repoName } = _parseGithubUrl(repoUrl);
   const repoInfo = await githubClient.repos.get({
     owner: githubUser,
     repo: repoName
-  });
-  const repoLanguages = await githubClient.repos.getLanguages({
+  }).then(repoInfo => repoInfo.data);
+  const repoContributors = await githubClient.repos.getContributors({
     owner: githubUser,
     repo: repoName
+  }).then(contributors => {
+    return contributors.data.map(contibutor => {
+      return {
+        userName: contibutor.login,
+        githubProfile: contibutor.url,
+        contributions: contibutor.contributions
+      }
+    });
   });
-
   return {
-    repoInfo,
-    name: repoInfo.data.name,
-    repoLanguages: repoLanguages.data
+    name: repoInfo.name,
+    description: repoInfo.description,
+    homepage: repoInfo.homepage,
+    stargazers_count: repoInfo.stargazers_count,
+    issues_url: repoInfo.issues_url,
+    license: repoInfo.license,
+    repoLanguages: repoInfo.language,
+    forks: repoInfo.forks,
+    open_issues: repoInfo.open_issues,
+    created_at: repoInfo.created_at,
+    updated_at: repoInfo.updated_at,
+    pushed_at: repoInfo.pushed_at,
+    git_url: repoInfo.git_url,
+    ssh_url: repoInfo.ssh_url,
+    clone_url: repoInfo.clone_url,
+    contributors: repoContributors
   };
 }
 
-function getRepoIssues(repoUrl, config) {
-  const githubClient = _getGithubClient(config);
+async function getRepoIssues(repoUrl, options) {
+  const githubClient = _getGithubClient(options);
 
   const { githubUser, repoName } = _parseGithubUrl(repoUrl);
-  return githubClient.issues.getForRepo({
+  const issues =  await githubClient.issues.getForRepo({
     owner: githubUser,
     repo: repoName,
-    state: 'open',
-    labels: config.githubLabels
+    state: options.GITHUB_ISSUE_STATE,
+    labels: options.GITHUB_ISSUE_LABELS
   }).then(info => info.data);
+
+  return issues.map(issue => {
+    let issueType;
+    let skillLevel;
+    let effort;
+    let impact;
+    let featured;
+
+    issue.labels.forEach(label => {
+      if(label.name.match(options.ISSUE_TYPE_REGEXP)) {
+        issueType = _getLabelValue(label.name, options.ISSUE_TYPE_REGEXP)
+      }
+      if(label.name.match(options.SKILL_LEVEL_REGEXP)) {
+        skillLevel = _getLabelValue(label.name, options.SKILL_LEVEL_REGEXP)
+      }
+      if(label.name.match(options.EFFORT_REGEXP)) {
+        effort = _getLabelValue(label.name, options.EFFORT_REGEXP)
+      }
+      if(label.name.match(options.IMPACT_REGEXP)) {
+        impact = _getLabelValue(label.name, options.IMPACT_REGEXP)
+      }
+      if(label.name.match(options.FEATURED_REGEXP)) {
+        featured = _getLabelValue(label.name, options.FEATURED_REGEXP)
+      }
+    });
+
+    return {
+      url: issue.html_url,
+      repository_url: issue.repository_url,
+      title: issue.title,
+      state: issue.state,
+      labels: issue.labels,
+      issueType: issueType,
+      skillLevel: skillLevel,
+      effort: effort,
+      impact: impact,
+      featured: featured
+    }
+  });
 }
 
 module.exports = {
