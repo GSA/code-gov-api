c0e2690d1bcb5a1eb45f57aaf98f465d28ae2663
Froilan Irizarry <froilan@froilanirizarry.me>
2018-02-07 12:20:15 -0500

    Change the way the app config is loaded - The config module now returns a getConfig function that accepts the environment value. This makes the config loading testable.

--- config/index.js
+++ config/index.js
@@ -1,21 +1,25 @@
 const path = require('path');
 const getProductionConfig = require('./prod');
 const getDevelopmentConfig = require('./dev');
-let config = {
-  prod_envs: ['prod', 'production', 'stag', 'staging']
-};
 
-if (config.prod_envs.includes(process.env.NODE_ENV)) {
-  config = Object.assign(config, getProductionConfig());
-} else {
-  config = Object.assign(config, getDevelopmentConfig());
+function getConfig(env) {
+  let config = {
+    prod_envs: ['prod', 'production', 'stag', 'staging']
+  };
+  if (config.prod_envs.includes(env)) {
+    config = Object.assign(config, getProductionConfig());
+  } else {
+    config = Object.assign(config, getDevelopmentConfig());
+  }
+  
+  config.AGENCY_ENDPOINTS_FILE = path.join(path.dirname(__dirname), config.AGENCY_ENDPOINTS_FILE);
+  config.REPORT_FILEPATH = path.join(path.dirname(__dirname), config.REPORT_FILEPATH);
+  config.DISCOVERED_DIR = path.join(path.dirname(__dirname), config.DISCOVERED_DIR);
+  config.FETCHED_DIR = path.join(path.dirname(__dirname), config.FETCHED_DIR);
+  config.DIFFED_DIR = path.join(path.dirname(__dirname), config.DIFFED_DIR);
+  config.FALLBACK_DIR = path.join(path.dirname(__dirname), config.FALLBACK_DIR);
+
+  return config;
 }
 
-config.AGENCY_ENDPOINTS_FILE = path.join(path.dirname(__dirname), config.AGENCY_ENDPOINTS_FILE);
-config.REPORT_FILEPATH = path.join(path.dirname(__dirname), config.REPORT_FILEPATH);
-config.DISCOVERED_DIR = path.join(path.dirname(__dirname), config.DISCOVERED_DIR);
-config.FETCHED_DIR = path.join(path.dirname(__dirname), config.FETCHED_DIR);
-config.DIFFED_DIR = path.join(path.dirname(__dirname), config.DIFFED_DIR);
-config.FALLBACK_DIR = path.join(path.dirname(__dirname), config.FALLBACK_DIR);
-
-module.exports = config;
+module.exports = getConfig;
