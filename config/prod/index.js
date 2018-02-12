52a97dc9c40f70a2beab9cd333f7bb24a48df7d9
Froilan Irizarry <froilan@froilanirizarry.me>
2018-02-12 16:14:59 -0500

    Fix cfenv usage - Removed the use of CLOUD_GOV variable for cfenv.islocal. This is a much better way to use the cfenv module and eliminates having to maintain another env var. - Added SWAGGER_HOST variable

--- config/prod/index.js
+++ config/prod/index.js
@@ -4,13 +4,14 @@ const jsonfile = require('jsonfile');
 
 function getProductionConfig(){
   const prodConfig = jsonfile.readFileSync(path.join(__dirname, 'index.json'))
-  if(process.env.CLOUD_GOV){
-    const appEnv = cfenv.getAppEnv();  
+  const appEnv = cfenv.getAppEnv();
+  if(!appEnv.isLocal){
     const elasticSearchCredentials = appEnv.getServiceCreds("code_gov_elasticsearch");
     
     prodConfig.ES_AUTH = `${elasticSearchCredentials.username}:${elasticSearchCredentials.password}`;
     prodConfig.ES_HOST = elasticSearchCredentials.hostname;
     prodConfig.ES_PORT = elasticSearchCredentials.port;
+    prodConfig.SWAGGER_HOST = appEnv.app.uris[0];
   }
 
   return prodConfig;
