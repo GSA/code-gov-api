95a9f9b4ae5c9cb87e1a43cdec699bb1121a92b0
Froilan Irizarry <froilan@froilanirizarry.me>
2018-03-27 16:05:08 -0400

    Initial commit for status indexer.

---
+++ services/indexer/status/index.js
@@ -0,0 +1,38 @@
+const AbstractIndexer = require("../abstract_indexer");
+const crypto = require("crypto");
+
+class StatusIndexer extends AbstractIndexer {
+  get LOGGER_NAME() {
+    return 'status-indexer';
+  }
+
+  constructor(adapter, params) {
+    super(adapter, params);
+  }
+
+  indexStatus (reporter) {
+    const idHash = crypto.createHash('md5')
+      .update(JSON.stringify(reporter.report), 'utf-8')
+      .digest('hex');
+
+    return this.indexExists()
+      .then(indexExists => {
+        if(indexExists) {
+          return this.deleteIndex();
+        }
+      })
+      .then(() => this.initIndex())
+      .then(() => this.initMapping())
+      .then(() => {
+        reporter.report.timestamp = (new Date()).toString();
+        return this.indexDocument({
+          "index": this.esIndex,
+          "type": this.esType,
+          "id": idHash,
+          "body": JSON.stringify(reporter.report)
+        });
+      });
+  }
+}
+
+module.exports = StatusIndexer;
