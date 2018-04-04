095bad75bec1d4a9922f7b0f541a1e6a88a976a4
Froilan Irizarry <froilan@froilanirizarry.me>
2018-04-04 19:27:32 -0400

    Refactor indexStatus - Removed all functionality except the indexDocument. This is possible because we are not creating a new index but using the passed index to save the status document

--- services/indexer/status/index.js
+++ services/indexer/status/index.js
@@ -15,43 +15,13 @@ class StatusIndexer extends AbstractIndexer {
       .update(JSON.stringify(reporter.report), 'utf-8')
       .digest('hex');
 
-    return this.indexExists()
-      .then(indexExists => {
-        if(indexExists) {
-          return this.deleteIndex();
-        }
-      })
-      .then(() => this.initIndex())
-      .then(() => this.initMapping())
-      .then(() => {
-        reporter.report.timestamp = (new Date()).toString();
-        return this.indexDocument({
-          "index": this.esIndex,
-          "type": this.esType,
-          "id": idHash,
-          "body": JSON.stringify(reporter.report)
-        });
-      })
-      .then(() => {
-        this.client.indices.getAlias({
-          name: this.esAlias
-        }, (err, response, status)=> {
-          if(status) {
-            this.logger.debug(`Status: ${status}`);
-          }
-          return response;
-        });
-      })
-      .then((oldStatusIndez) => {
-        this.client.updateAliases({
-          body: {
-            actions: [
-              { remove: { index: oldStatusIndez, alias: this.esAlias } },
-              { add: { index: this.esIndex, alias: this.esAlias } }
-            ]
-          }
-        });
-      });
+    reporter.report.timestamp = (new Date()).toString();
+    return this.indexDocument({
+      "index": this.esIndex,
+      "type": this.esType,
+      "id": idHash,
+      "body": JSON.stringify(reporter.report)
+    });
   }
 }
 
