6e6bb1b02e89d8f89dd247c8b62edfbb8d9b866d
Froilan Irizarry <froilan@froilanirizarry.me>
2018-06-25 12:47:28 -0400

    Complete refactor of tests and assertions

--- test/unit/config-loading.test.js
+++ test/unit/config-loading.test.js
@@ -1,17 +1,45 @@
-const getConfig = require('../../config');
-const chai = require('chai');
-const should = chai.should();
+const getConfig = require('../../../config');
+const should = require('chai').should();
+const path = require('path');
 
 describe('Load config', function() {
+
   it('should load development config', function() {
     const config = getConfig('development');
+    supportedSchemaVersions = [
+      '1.0.0',
+      '1.0.1',
+      '2.0.0'
+    ];
 
-    config.ES_HOST.should.be.equal('localhost');
+    config.ES_HOST.should.be.equal('http://localhost:9200');
+    config.PORT.should.be.equal('3000')
+    config.USE_HSTS.should.be.equal(false)
+    config.HSTS_MAX_AGE.should.be.equal(31536000)
+    config.HSTS_PRELOAD.should.be.equal(false)
+    config.LOGGER_LEVEL.should.be.equal('DEBUG')
+    should.exist(config.SWAGGER_DOCUMENT)
+
   });
   it('should load production config', function() {
-    const config = getConfig('production');
+    process.env.LOGGER_LEVEL='TRACE'
+    process.env.USE_HSTS=true
+    process.env.HSTS_MAX_AGE=31536333
+    process.env.HSTS_PRELOAD=false
+    process.env.PORT=8888
+    process.env.ES_URI='https://somewhere.com/es'
+    const config = getConfig({
+      env: 'production',
+      dotEnvPath: path.join(__dirname, '.env-test')
+    });
 
-    config.USE_HSTS.should.be.equal(true);
+    config.ES_HOST.should.be.equal('https://somewhere.com/es');
+    config.PORT.should.be.equal('8888')
+    config.USE_HSTS.should.be.equal(true)
+    config.HSTS_MAX_AGE.should.be.equal(31536333)
+    config.HSTS_PRELOAD.should.be.equal(false)
+    config.LOGGER_LEVEL.should.be.equal('TRACE')
+    should.exist(config.SWAGGER_DOCUMENT)
   });
 });
 
