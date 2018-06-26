189ce51c52b87bc1cf729ac45bfadf59ec51ae66
Froilan Irizarry <froilan@froilanirizarry.me>
2018-06-26 15:26:45 -0400

    Fixed broken test

--- test/unit/config-loading.test.js
+++ test/unit/config-loading.test.js
@@ -13,33 +13,29 @@ describe('Load config', function() {
     ];
 
     config.ES_HOST.should.be.equal('http://localhost:9200');
-    config.PORT.should.be.equal('3000')
-    config.USE_HSTS.should.be.equal(false)
-    config.HSTS_MAX_AGE.should.be.equal(31536000)
-    config.HSTS_PRELOAD.should.be.equal(false)
-    config.LOGGER_LEVEL.should.be.equal('DEBUG')
-    should.exist(config.SWAGGER_DOCUMENT)
+    config.USE_HSTS.should.be.equal(false);
+    config.HSTS_MAX_AGE.should.be.equal(31536000);
+    config.HSTS_PRELOAD.should.be.equal(false);
+    config.LOGGER_LEVEL.should.be.equal('DEBUG');
+    should.exist(config.SWAGGER_DOCUMENT);
 
   });
   it('should load production config', function() {
-    process.env.LOGGER_LEVEL='TRACE'
-    process.env.USE_HSTS=true
-    process.env.HSTS_MAX_AGE=31536333
-    process.env.HSTS_PRELOAD=false
-    process.env.PORT=8888
-    process.env.ES_URI='https://somewhere.com/es'
-    const config = getConfig({
-      env: 'production',
-      dotEnvPath: path.join(__dirname, '.env-test')
-    });
+    process.env.LOGGER_LEVEL='TRACE';
+    process.env.USE_HSTS=true;
+    process.env.HSTS_MAX_AGE=31536333;
+    process.env.HSTS_PRELOAD=false;
+    process.env.PORT=8888;
+    process.env.ES_URI='https://somewhere.com/es';
+    const config = getConfig('production');
 
     config.ES_HOST.should.be.equal('https://somewhere.com/es');
-    config.PORT.should.be.equal('8888')
-    config.USE_HSTS.should.be.equal(true)
-    config.HSTS_MAX_AGE.should.be.equal(31536333)
-    config.HSTS_PRELOAD.should.be.equal(false)
-    config.LOGGER_LEVEL.should.be.equal('TRACE')
-    should.exist(config.SWAGGER_DOCUMENT)
+    config.PORT.should.be.equal('8888');
+    config.USE_HSTS.should.be.equal(true);
+    config.HSTS_MAX_AGE.should.be.equal(31536333);
+    config.HSTS_PRELOAD.should.be.equal(false);
+    config.LOGGER_LEVEL.should.be.equal('TRACE');
+    should.exist(config.SWAGGER_DOCUMENT);
   });
 });
 
