af3f214c3f9080740103de331417e676a02b1ec1
Froilan Irizarry <froilan@froilanirizarry.me>
2018-10-02 21:06:41 -0400

    Refactor - Removed unused require statements - Removed ElasticSearchLogger. - IndexOptimizer is no longer extending AbstractIndexTool. - Refactored forceMerge to use async await. - Refactored init function to use async await.

--- services/indexer/index_optimizer.js
+++ services/indexer/index_optimizer.js
@@ -1,14 +1,4 @@
-const AbstractIndexTool   = require("./abstract_index_tool");
 const Logger              = require("../../utils/logger");
-
-/* eslint-disable */
-const ElasticSearch       = require("elasticsearch");
-class ElasticSearchLogger extends Logger {
-  get DEFAULT_LOGGER_NAME() {
-    return "elasticsearch";
-  }
-}
-/* eslint-enable */
 
 /**
  * Class for optimizing ElasticSearch Indexes
@@ -16,10 +6,6 @@ class ElasticSearchLogger extends Logger {
  * @class AliasSwapper
  */
 class IndexOptimizer {
-
-  get LOGGER_NAME() {
-    return "index-optimizer";
-  }
 
   /**
    * Creates an instance of AliasSwapper.
@@ -37,18 +23,20 @@ class IndexOptimizer {
    * searches.
    *
    * @param {any} indexName The index to optimize.
-   * @param {any} callback
    */
-  forceMerge(indexName, callback) {
+  async forceMerge(indexName) {
     this.logger.info(`Optimizing Index (${indexName})`);
 
-    this.adapter.forceMerge({
-      maxNumSegments: 1,
-      index: indexName,
-      requestTimeout: 90000
-    })
-      .then(callback(null))
-      .catch(error => callback(error));
+    try {
+      await this.adapter.forceMerge({
+        maxNumSegments: 1,
+        index: indexName,
+        requestTimeout: 90000
+      });
+    } catch(error) {
+      this.logger.trace(error);
+      throw error;
+    }
   }
 
   /**
@@ -59,20 +47,18 @@ class IndexOptimizer {
    * @param {any} repoIndexInfo Information about the index and alias for repos
    * @param {any} callback
    */
-  static init(adapter, repoIndexInfo, callback=undefined) {
+  static async init(adapter, repoIndexInfo) {
 
     let optimizer = new IndexOptimizer(adapter);
     optimizer.logger.info(`Starting index optimization.`);
-
-    optimizer.forceMerge(repoIndexInfo.esIndex, (err) => {
-      if(err) {
-        optimizer.logger.error(err);
-      }
+    try {
+      await optimizer.forceMerge(repoIndexInfo.esIndex);
       optimizer.logger.info(`Finished optimizing indices.`);
-      if( callback && typeof callback === 'function') {
-        return callback(repoIndexInfo, err);
-      }
-    });
+    } catch(error) {
+      optimizer.logger.trace(error);
+      throw error;
+    }
+
   }
 
 }
